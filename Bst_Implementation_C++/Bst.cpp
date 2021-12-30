a17f209c3e5266eb6a942a355060f29b1f5757d3
Ajit <singh2003ajit@gmail.com>
2021-12-30 08:17:52 +0530

    Rearranged Some functions and improved code

--- Bst_Implementation_C++/Bst.cpp
+++ Bst_Implementation_C++/Bst.cpp
@@ -1,6 +1,7 @@
 #include "Bst.h"
 
-BstNode *BST :: _createNode(int data)
+// Private/Utility Functions
+BstNode *BST :: _create_Node_Util(int data)
 {
 	BstNode *newNode = new BstNode();
 	if (!newNode)
@@ -10,22 +11,22 @@ BstNode *BST :: _createNode(int data)
 	return newNode;
 }
 
-BstNode *BST :: _insert(BstNode *root, int data)
+BstNode *BST :: _insert_Util(BstNode *root, int data)
 {
 	if (root == NULL) {
-		root = _createNode(data);
+		root = _create_Node_Util(data);
 	}
 	else if (data <= root->data) {
-		root->left = _insert(root->left, data);
+		root->left = _insert_Util(root->left, data);
 	}
 	else {
-		root->right = _insert(root->right, data);
+		root->right = _insert_Util(root->right, data);
 	}
 
 	return root;
 }
 
-bool BST :: _search(BstNode *root, int data)
+bool BST :: _search_Util(BstNode *root, int data)
 {
 	if (!root) {
 		return false;
@@ -35,31 +36,222 @@ bool BST :: _search(BstNode *root, int data)
 		return true;
 	}
 	else if (data < root->data) {
-		return _search(root->left, data);
+		return _search_Util(root->left, data);
 	}
 	else {
-		return _search(root->right, data);
+		return _search_Util(root->right, data);
 	}
 
 	return false;
 }
 
+BstNode *BST :: _delete_Util(BstNode *root, int data)
+{
+	if (_root == NULL)
+		return NULL;
+	else if (data < root->data)
+		root->left = _delete_Util(root->left, data);
+	else if (data > root->data)
+		root->right = _delete_Util(root->right, data);
+	else
+	{
+		// Case 1: No child
+		if (root->left == NULL && root->right == NULL)
+		{
+			delete root;
+			root = NULL;
+		}
+		// Case 2: 1 Child
+		else if (root->left == NULL)
+		{
+			BstNode *temp = root;
+			root = root->right;
+			delete temp;
+		}
+		else if (root->right == NULL)
+		{
+			BstNode *temp = root;
+			root = root->left;
+			delete temp;
+		}
+		// Case 3: 2 Children
+		else
+		{
+			BstNode *temp = _find_Min_Util(root->right);
+			root->data = temp->data;
+			root->right = _delete_Util(root->right, temp->data);
+		}
+	}
+	return root;
+}
+
+BstNode* BST :: _find_Node_Util(int data)
+{
+	BstNode* temp = _root;
+	while (temp != NULL)
+	{
+		if (data = temp->data)
+			return temp;
+		else if (data < temp->data)
+			temp = temp->left;
+		else
+			temp = temp->right;
+	}
+	return temp;
+}
+
+int BST :: _find_Height_Util(BstNode *root)
+{
+	if (root == NULL)
+		return -1;
+
+	int height = _max_Util( _find_Height_Util(root->left), _find_Height_Util(root->right) ) + 1;
+	return height;
+}
+
+BstNode* BST :: _find_Min_Util(BstNode *root)
+{
+	if (!root)
+		return NULL;
+	while (root->left != NULL)
+		root = root->left;
+	return root;
+}
+
+BstNode* BST :: _find_Max_Util(BstNode *root)
+{
+	if (!root)
+		return NULL;
+	while (root->right != NULL)
+		root = root->right;
+	return root;
+}
+
+int BST :: _max_Util(int a, int b)
+{
+	return (a > b ? a : b);
+}
+
+void BST :: _Preorder_Traversal(BstNode* root)
+{
+	if (root == NULL)
+		return;
+
+	std::cout << root->data << " ";
+	_Preorder_Traversal(root->left);
+	_Preorder_Traversal(root->right);
+}
+
+void BST :: _Inorder_Traversal(BstNode* root)
+{
+	if (root == NULL)
+		return;
+
+	_Inorder_Traversal(root->left);
+	std::cout << root->data << " ";
+	_Inorder_Traversal(root->right);
+}
+
+void BST :: _Postorder_Traversal(BstNode* root)
+{
+	if (root == NULL)
+		return;
+	
+	_Postorder_Traversal(root->left);
+	_Postorder_Traversal(root->right);
+	std::cout << root->data << " ";
+}
+
+// Public Functions
 bool BST :: insert(int data)
 {
-	_root = _insert(_root, data);
-	if (_root)
+	BstNode* temp = _insert_Util(_root, data);
+	if (temp) {
+		_root = temp;
 		return true;
-
+	}
 	return false;
 }
 
 bool BST :: search(int data)
 {
-	return _search(_root, data);
+	return _search_Util(_root, data);
 }
 
-BstNode *BST :: returnRoot()
+bool BST :: remove(int data)
 {
-	return _root;
+	BstNode* temp = _delete_Util(_root, data);
+	if (temp) {
+		_root = temp;
+		return true;
+	}
+	return false;
+}
+
+int BST :: FindMin()
+{
+	BstNode* node = _find_Min_Util(_root);
+	if (node)
+		return node->data;
+	return INT_MIN;
+}
+
+int BST :: FindMax()
+{
+	BstNode* node = _find_Max_Util(_root);
+	if (node)
+		return node->data;
+	return INT_MIN;
+}
+
+int BST :: FindHeight()
+{
+	int height = _find_Height_Util(_root);
+	if (height < 0)
+		return INT_MIN;
+	return height;
+}
+
+void BST :: Preorder_Traversal()
+{
+	_Preorder_Traversal(_root);
+}
+
+void BST :: Inorder_Traversal()
+{
+	_Inorder_Traversal(_root);
+}
+
+void BST :: Postorder_Traversal()
+{
+	_Postorder_Traversal(_root);
+}
+
+int BST :: InorderSuccessor(int data)
+{
+	BstNode *current = _find_Node_Util(data);
+	if (current == NULL) return INT_MIN;
+	// Case 1: Node has right subtree
+	if (current->right != NULL)
+	{
+		return _find_Min_Util(current->right)->data;
+	}
+	// Case 2: Node has no right subtree
+	else
+	{
+		BstNode *successor = NULL;
+		BstNode *ancestor = _root;
+		while (ancestor != current)
+		{
+			if (current->data < ancestor->data)
+			{
+				successor = ancestor;
+				ancestor = ancestor->left;
+			}
+			else
+				ancestor = ancestor->right;
+		}
+		return ancestor->data;
+	}
 }
 
--- Bst_Implementation_C++/Bst.h
+++ Bst_Implementation_C++/Bst.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 struct BstNode
 {
@@ -14,11 +15,27 @@ private:
 	BstNode *_root = NULL;
 
 	// Functions
-	BstNode *_createNode(int);
-	BstNode *_insert(BstNode *, int);
-	bool _search(BstNode *, int);
+	BstNode *_create_Node_Util(int);
+	BstNode *_insert_Util(BstNode *, int);
+	bool _search_Util(BstNode *, int);
+	BstNode *_delete_Util(BstNode *, int);
+	BstNode* _find_Node_Util(int);
+	int _find_Height_Util(BstNode *);
+	BstNode* _find_Min_Util(BstNode *);
+	BstNode* _find_Max_Util(BstNode *);
+	int _max_Util(int, int);
+	void _Preorder_Traversal(BstNode *);
+	void _Inorder_Traversal(BstNode *);
+	void _Postorder_Traversal(BstNode *);
 public:
 	bool insert(int);
 	bool search(int);
-	BstNode *returnRoot();
+	bool remove(int);
+	int FindMin();
+	int FindMax();
+	int FindHeight();
+	void Preorder_Traversal();
+	void Inorder_Traversal();
+	void Postorder_Traversal();
+	int InorderSuccessor(int);
 };
